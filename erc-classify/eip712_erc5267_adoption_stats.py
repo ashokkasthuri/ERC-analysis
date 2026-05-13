1b9c77e0684ceb1dfd85e8aa983a12aa9164d022
ashokkasthuri <ashokraj.kasthuri@gmail.com>
2026-05-13 15:40:18 +0800

    .enf error fix

--- erc-classify/eip712_erc5267_adoption_stats.py
+++ erc-classify/eip712_erc5267_adoption_stats.py
@@ -24,7 +24,7 @@ import requests
 import pandas as pd
 
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from tqdm import tqdm
 from typing import Optional, Tuple
 
